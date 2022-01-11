7dcb6b7a831be97eb9b9f6d978a3f92837b9e106
fahad alajmi <fahadalajmi126@gmail.com>
2022-01-11 20:20:25 +0300

    ..

--- index.js
+++ index.js
@@ -10,7 +10,27 @@
  * Open the live server, and see the logs in the console of the browser
  *
  */
-
+const movie1 ={
+    id:1, 
+    name: "whiplash",
+    image:"https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/product_image_aspect_switcher_170w/public/product-images/csm-movie/whiplash-poster.jpg?itok=Ik3xmD7f",
+    rating:8.5
+};
+const movie2 ={
+    id:2, 
+    name: "diehard", 
+    image:"https://static.metacritic.com/images/products/movies/2/b11ef7a183c8ca854c5cf42675e61a36-250h.jpg",
+    rating:8.2
+};
+const movie3 ={
+    id:3, 
+    name:"the usual suspects", 
+    image:"https://en.wikipedia.org/wiki/File:Usual_suspects_ver1.jpg",
+    rating:8.5 
+}
+console.log(movie1);
+console.log(movie2);
+console.log(movie3);
 // add the movies variables here ....
 
 /** Task 2:
@@ -29,9 +49,29 @@
  *    },
  * ]
  */
-
+const movies =[
+    {
+        id:1, 
+        name: "whiplash",
+        image:"https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/product_image_aspect_switcher_170w/public/product-images/csm-movie/whiplash-poster.jpg?itok=Ik3xmD7f",
+        rating:8.5
+    },{
+    
+        id:2, 
+        name: "diehard", 
+        image:"https://static.metacritic.com/images/products/movies/2/b11ef7a183c8ca854c5cf42675e61a36-250h.jpg",
+        rating:8.2
+    },
+    {
+        id:3, 
+        name:"the usual suspects", 
+        image:"https://en.wikipedia.org/wiki/File:Usual_suspects_ver1.jpg",
+        rating:8.5 
+    }
+];
 /** TASK 3:
  * Create a new file call it `movies.json`
  * Copy paste the content of the array inside the json file
  * Make sure it's a json format.
  */
+
